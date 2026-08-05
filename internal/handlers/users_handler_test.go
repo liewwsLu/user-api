e4f1c930d05c545af6ed523119c71f20011fe371
liewwsLu <egorrbusiness@yandex.ru>
2026-08-05 18:05:12 +0300

    test: add create user handler success test

--- internal/handlers/users_handler_test.go
+++ internal/handlers/users_handler_test.go
@@ -5,13 +5,19 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"user-api/internal/models"
 )
 
 type fakeUserStorage struct {
-	users   []models.User
-	listErr error
+	users        []models.User
+	listErr      error
+	createErr    error
+	createdUser  models.User
+	gotName      string
+	gotEmail     string
+	createCalled bool
 }
 
 func (f *fakeUserStorage) ListUsers() ([]models.User, error) {
@@ -23,7 +29,10 @@ func (f *fakeUserStorage) FindUserByID(id int) (models.User, error) {
 }
 
 func (f *fakeUserStorage) CreateUser(name, email string) (models.User, error) {
-	panic("not implemented")
+	f.createCalled = true
+	f.gotEmail = email
+	f.gotName = name
+	return f.createdUser, f.createErr
 }
 
 func (f *fakeUserStorage) UpdateUser(id int, name, email string) (models.User, error) {
@@ -88,5 +97,53 @@ func TestUsersHandlerReturnsInternalServerError(t *testing.T) {
 	}
 	if gotErr["error"] != storageErr.Error() {
 		t.Errorf("UsersHandler() error = %q, want %q", gotErr["error"], storageErr.Error())
+	}
+}
+
+func TestUsersHandlerCreateUser(t *testing.T) {
+	wantUser := models.User{
+		ID:    1,
+		Name:  "Dima",
+		Email: "dima@yande.ri",
+	}
+	fake := &fakeUserStorage{createdUser: wantUser}
+	h := New(fake)
+	request := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(`{"name":"Dima","email":"dima@yande.ri"}`))
+	recorder := httptest.NewRecorder()
+	h.UsersHandler(recorder, request)
+	if recorder.Code != http.StatusCreated {
+		t.Errorf("CreateUser() status: %d, want: %d", recorder.Code, http.StatusCreated)
+	}
+	if !fake.createCalled {
+		t.Fatalf("CreateUser was not called")
+	}
+	if fake.gotName != wantUser.Name {
+		t.Errorf(
+			"CreateUser() name = %q, want %q",
+			fake.gotName,
+			wantUser.Name,
+		)
+	}
+
+	if fake.gotEmail != wantUser.Email {
+		t.Errorf(
+			"CreateUser() email = %q, want %q",
+			fake.gotEmail,
+			wantUser.Email,
+		)
+	}
+	var gotUser models.User
+	err := json.NewDecoder(recorder.Body).Decode(&gotUser)
+	if err != nil {
+		t.Fatalf("decode response body: %v", err)
+	}
+	if gotUser.ID != wantUser.ID {
+		t.Errorf("response ID = %d, want %d", gotUser.ID, wantUser.ID)
+	}
+	if gotUser.Name != wantUser.Name {
+		t.Errorf("Name() got: %q, want: %q", gotUser.Name, wantUser.Name)
+	}
+	if gotUser.Email != wantUser.Email {
+		t.Errorf("Email() got: %q, want: %q", gotUser.Email, wantUser.Email)
 	}
 }
